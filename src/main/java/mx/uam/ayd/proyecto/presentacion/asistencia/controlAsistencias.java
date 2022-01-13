e941934bc10d365645c198b0ad8edb48f68ea3a5
usuario <usuario@DESKTOP-UF10708>
2022-01-13 12:22:05 -0600

    importacion de paquetes y creacion de variables

--- src/main/java/mx/uam/ayd/proyecto/presentacion/asistencia/controlAsistencias.java
+++ src/main/java/mx/uam/ayd/proyecto/presentacion/asistencia/controlAsistencias.java
@@ -1,5 +1,27 @@
 package mx.uam.ayd.proyecto.presentacion.asistencia;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import mx.uam.ayd.proyecto.negocio.ServicioAsistencia;
+import mx.uam.ayd.proyecto.negocio.ServicioEmpleado;
+import mx.uam.ayd.proyecto.negocio.modelo.Asistencia;
+import mx.uam.ayd.proyecto.negocio.modelo.Empleado;
+import mx.uam.ayd.proyecto.presentacion.controlEmpleados.ControlEmpleados;
+import mx.uam.ayd.proyecto.presentacion.inicioSesion.ControlInicioSesion;
+import mx.uam.ayd.proyecto.presentacion.monitoreo.ControlMonitoreo;
+@Component
 
 public class controlAsistencias {
-
+	@Autowired
+	private ControlMonitoreo controlMonitoreo;
+	@Autowired
+	private ControlInicioSesion controlInicioSesion;
+	@Autowired
+	private ventanaAsistencias ventana;
+	@Autowired
+	private ServicioAsistencia servicioAsistencia;
+	@Autowired
+	private ServicioEmpleado servicioEmpleado;
 }
